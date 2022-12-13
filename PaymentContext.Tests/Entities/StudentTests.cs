37adc2d3e5c2aea63ea9a7c98d0732dcbd97dbbc
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-12-13 14:55:13 -0300

    Refactoring PaymentContext.Domain Entities and realizing some tests

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void AddSubscription() 
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Andre", "Baltieri", "192394014", "andre@balta.io");
+        student.AddSubscription(subscription);
+
+        // var payment = new PaypalPayment();
+    }
+}
